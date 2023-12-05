9e0362f90178013e33de1a1ca32be18a2cd83d0a
simone-art <simoneperiodista3@gmail.com>
2023-12-05 11:13:08 -0300

    Criando o KafkaProducerMessage pra enviar as mensagens no tópico

---
+++ api/src/main/java/com/portal/api/message/KafkaProducerMessage.java
@@ -0,0 +1,22 @@
+package com.portal.api.message;
+
+import com.portal.api.dto.CarPostDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducerMessage {
+
+    @Autowired
+    // Kaftkatemplate faz o envio das mensagens pra dentro dos tópicos
+    // É um método do Kafka
+    private KafkaTemplate<String, CarPostDto> kafkaTemplate;
+
+    private final String KAFKA_TOPIC = "car-post-topic";
+
+    public void sendMessage(CarPostDto carPostDto){
+        kafkaTemplate.send(KAFKA_TOPIC, carPostDto); 
+
+    }
+}
