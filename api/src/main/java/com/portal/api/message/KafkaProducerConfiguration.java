14f5d8e00d23a041974411b5535f4aa07c468c85
simone-art <simoneperiodista3@gmail.com>
2023-12-05 10:54:06 -0300

    Criando o Kafka Producer

---
+++ api/src/main/java/com/portal/api/message/KafkaProducerConfiguration.java
@@ -0,0 +1,47 @@
+package com.portal.api.message;
+
+
+
+import com.portal.api.dto.CarPostDto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.stereotype.Component;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+@Component
+public class KafkaProducerConfiguration {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    // Servidor que executa o clúster do Apache Kafka
+    private String bootstrapServer;
+
+    @Bean
+    public ProducerFactory<String, CarPostDto> userProducerFactory() {
+        // Configurações que permitem conectar o Kafka ao tópico
+        Map<String, Object> configProps = new HashMap<>();
+
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+
+    }
+
+    @Bean
+    public KafkaTemplate<String, CarPostDto> userKafkaTemplate(){
+        return new KafkaTemplate<>(userProducerFactory());
+    }
+}
